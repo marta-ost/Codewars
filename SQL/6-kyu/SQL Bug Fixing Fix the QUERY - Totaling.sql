909f266c0287861e632e8ffc4c1a9e0621127685
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-11-26 19:30:17 +0100

    create SQL Bug Fixing Fix the QUERY - Totaling.sql

---
+++ SQL/6-kyu/SQL Bug Fixing Fix the QUERY - Totaling.sql
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+Oh no! Timmys been moved into the database divison of his software company but as we know Timmy loves making mistakes. Help Timmy keep his job by fixing his query...
+
+Timmy works for a statistical analysis company and has been given a task of totaling the number of sales on a given day grouped by each department name and then each day.
+
+Resultant table:
+day (type: date) {group by} [order by asc]
+department (type: text) {group by} [In a real world situation it is bad practice to name a column after a table]
+sale_count (type: int)
+Tables and relationship below:
+*/
+
+SELECT 
+  CAST(s.transaction_date AS DATE) AS day,
+  d.name AS department,
+  COUNT(s.id) AS sale_count
+FROM department d
+    JOIN sale s on d.id = s.department_id
+GROUP BY day, department
+ORDER BY day;
