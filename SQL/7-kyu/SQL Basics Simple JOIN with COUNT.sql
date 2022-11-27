1e4b959a85d3520585a25ed4f93142a410b4e87a
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-11-27 22:54:40 +0100

    create SQL Basics Simple JOIN with COUNT.sql

---
+++ SQL/7-kyu/SQL Basics Simple JOIN with COUNT.sql
@@ -0,0 +1,24 @@
+/*
+DESCRIPTION:
+For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return the COUNT of the toys
+
+people table schema
+id
+name
+toys table schema
+id
+name
+people_id
+You should return all people fields as well as the toy count as "toy_count".
+
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+*/
+
+SELECT 
+  p.id, 
+  p.name, 
+  COUNT(*) AS toy_count
+FROM people AS p
+JOIN toys AS t
+  ON p.id = t.people_id
+GROUP BY p.id;
