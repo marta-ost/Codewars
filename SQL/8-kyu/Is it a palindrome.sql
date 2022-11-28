6414df13985448d34eaea270895be4996cb83b6f
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-11-28 22:19:53 +0100

    refactor Is it a palindrome.sql

--- SQL/8-kyu/Is it a palindrome.sql
+++ SQL/8-kyu/Is it a palindrome.sql
@@ -6,7 +6,5 @@ Write a function that checks if a given string (case insensitive) is a palindrom
 -- # write your SQL statement here: you are given a table 'ispalindrome' with column 'str', return a table with column 'str' and your result in a column named 'res'.
 SELECT 
   str, 
-  CASE WHEN LOWER(REVERSE(str)) = LOWER(str)
-       THEN true 
-       ELSE false END AS res
+  LOWER(REVERSE(str)) = LOWER(str) AS res
 FROM ispalindrome;
