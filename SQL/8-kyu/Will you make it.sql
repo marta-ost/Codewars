f96f30c1354f961cdea80b52af2f025be35a0065
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-11-28 22:18:26 +0100

    refactor Will you make it.sql

--- SQL/8-kyu/Will you make it.sql
+++ SQL/8-kyu/Will you make it.sql
@@ -12,7 +12,5 @@ SELECT
   distance_to_pump,
   mpg,
   fuel_left,
-  CASE WHEN fuel_left * mpg >= distance_to_pump
-       THEN true
-       ELSE false END AS res
+  fuel_left * mpg >= distance_to_pump AS res
 FROM zerofuel;
