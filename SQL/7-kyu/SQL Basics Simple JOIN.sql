927c69d7f54d3878727d797c4bac9e3a854f961e
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-11-29 13:24:41 +0100

    create SQL Basics Simple JOIN.sql

---
+++ SQL/7-kyu/SQL Basics Simple JOIN.sql
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+For this challenge you need to create a simple SELECT statement that will return all columns from the products table, and join to the companies table so that you can return the company name.
+
+products table schema
+id
+name
+isbn
+company_id
+price
+companies table schema
+id
+name
+You should return all product fields as well as the company name as "company_name".
+
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+*/
+
+SELECT
+  p.id,
+  p.name,
+  p.isbn,
+  p.company_id,
+  p.price,
+  c.name AS company_name
+FROM products AS p
+JOIN companies AS c
+  ON c.id = p.company_id;
