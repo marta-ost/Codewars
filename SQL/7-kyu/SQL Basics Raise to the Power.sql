39c7a004204a57c20f54002f2c2d37544ead25d5
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-03 14:40:04 +0100

    refactor SQL Basics Raise to the Power.sql

--- SQL/7-kyu/SQL Basics Raise to the Power.sql
+++ SQL/7-kyu/SQL Basics Raise to the Power.sql
@@ -9,5 +9,6 @@ number2
 Return a table with a single column result which is the output of number1 raised to the power of number2.
 */
 
-SELECT POWER(number1, number2) AS result
+SELECT 
+  POWER(number1, number2) AS result
 FROM decimals;
