0cc3b675c39202db74176c813b4a74e203f1a6cb
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-08 22:06:49 +0100

    refactor String Templates - Bug Fixing 5.java

--- 8-kyu/String Templates - Bug Fixing 5.java
+++ 8-kyu/String Templates - Bug Fixing 5.java
@@ -5,17 +5,6 @@ Oh no! Timmy hasn't followed instructions very carefully and forgot how to use t
 
 public class Kata {
   public static String buildString(String... args) {
-    StringBuilder string = new StringBuilder();
-    
-    string.append("I like ");
-    for (int i = 0; i < args.length; i++) {
-      if (i == args.length - 1) {
-        string.append(args[i]);
-      } else {
-        string.append(args[i]).append(", ");
-      }
-    }
-    string.append("!");
-    return string.toString();
+    return "I like " + String.join(", ", args) + "!";
   }
 }
