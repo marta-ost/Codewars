1ef91703a0814d1bb17c54f481b52cb0df4f88b2
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-08 21:13:41 +0100

    create Function 2 - squaring an argument.java

---
+++ 8-kyu/Function 2 - squaring an argument.java
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+public class Kata {
+  public static int square(int n){
+    return (int) Math.pow(n, 2);
+  }
+}
