420c1d689a2b8359b91b644a9fb8bcceabec1ecd
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-09 12:38:00 +0100

    create Reversed Strings.java

---
+++ 8-kyu/Reversed Strings.java
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+public class Kata {
+  public static String solution(String str) {
+    return new StringBuilder(str).reverse().toString();
+  }
+}
