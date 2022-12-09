ff71856b05989235931f09e810a9abf4e54902ee
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-09 17:49:59 +0100

    refactor Return the day.java

--- 8-kyu/Return the day.java
+++ 8-kyu/Return the day.java
@@ -14,15 +14,15 @@ Otherwise returns "Wrong, please enter a number between 1 and 7"
 
 public class DayOfWeek {
   public static String getDay(int n) {
-    switch(n) {
-        case 1 : return "Sunday";
-        case 2 : return "Monday";
-        case 3 : return "Tuesday";
-        case 4 : return "Wednesday";
-        case 5 : return "Thursday";
-        case 6 : return "Friday";
-        case 7 : return "Saturday";
-        default: return "Wrong, please enter a number between 1 and 7";
-    }
+    return switch(n) {
+        case 1 -> "Sunday";
+        case 2 -> "Monday";
+        case 3 -> "Tuesday";
+        case 4 -> "Wednesday";
+        case 5 -> "Thursday";
+        case 6 -> "Friday";
+        case 7 -> "Saturday";
+        default -> "Wrong, please enter a number between 1 and 7";
+    };
   }
 }
