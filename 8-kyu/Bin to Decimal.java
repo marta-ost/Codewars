549fbbb5288c7b571a16eb6cc1a8c9642fd4e232
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-12 20:46:41 +0100

    create Bin to Decimal.java

---
+++ 8-kyu/Bin to Decimal.java
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+public class Converter {
+  public static int binToDecimal(String inp) {
+    return Integer.parseInt(inp, 2);
+  }
+}
