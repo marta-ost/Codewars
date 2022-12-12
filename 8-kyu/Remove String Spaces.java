ea3c7152359a2dc4161f4c203bf637f7b839ce95
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-12 13:10:48 +0100

    create Remove String Spaces.java

---
+++ 8-kyu/Remove String Spaces.java
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+public class Kata {
+  public static String noSpace(final String x) {
+    return x.replace(" ", "");
+  }
+}
