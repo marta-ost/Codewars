f4fd620f19dda7f3ada0adab3cbc3706ec024b19
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-13 16:06:07 +0100

    create Fake Binary.java

---
+++ 8-kyu/Fake Binary.java
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+*/
+
+public class FakeBinary {
+  public static String fakeBin(String numberString) {
+    return numberString.replaceAll("[1234]", "0").replaceAll("[56789]", "1");
+  }
+}
