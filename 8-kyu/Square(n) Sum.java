636946554c872218d959bc9783dac32714002443
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-13 16:16:01 +0100

    create Square(n) Sum.java

---
+++ 8-kyu/Square(n) Sum.java
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+*/
+
+import java.util.Arrays;
+
+public class Kata {
+  public static int squareSum(int[] n) { 
+    return Arrays.stream(n)
+      .map(x -> x * x)
+      .sum();
+  }
+}
