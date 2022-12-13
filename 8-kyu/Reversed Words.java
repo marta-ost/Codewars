33327e612ab101224ca541cff40a43eabc365b1e
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-13 18:54:57 +0100

    refactor Reversed Words.java

--- 8-kyu/Reversed Words.java
+++ 8-kyu/Reversed Words.java
@@ -7,13 +7,14 @@ Example(Input --> Output):
 "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 */
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+
 public class ReverseWords {
   public static String reverseWords(String str) {
-    StringBuilder reversed = new StringBuilder();
-    String[] words = str.split(" ");
-    for (int i = words.length - 1; i >= 0; i--) {
-      reversed.append(words[i]).append(" ");
+     List<String> words = Arrays.asList(str.split(" "));
+     Collections.reverse(words);
+     return String.join(" ", words);
     }
-    return reversed.substring(0, reversed.length() - 1);
-  }
 }
