f0f34c50571f63f0537e4ad0dc46b207c57b7e5e
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-13 17:10:13 +0100

    refactor What is between.java

--- 8-kyu/What is between.java
+++ 8-kyu/What is between.java
@@ -9,12 +9,10 @@ b = 4
 --> [1, 2, 3, 4]
 */
 
+import static java.util.stream.IntStream.rangeClosed;
+
 public class Kata {
   public static int[] between(int a, int b) {
-    int[] array = new int[b - a + 1];
-    for (int i = a; i <= b; i++) {
-      array[i - a] = i;
-    }
-    return array;
+    return rangeClosed(a, b).toArray();
   }
 }
