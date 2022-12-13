906fbc9884592508f32716b97d190c544d5ec0fb
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-13 16:33:57 +0100

    create Convert a Boolean to a String.java

---
+++ 8-kyu/Convert a Boolean to a String.java
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+public class BooleanToString {
+  public static String convert(boolean b){
+    return b ? "true" : "false";
+  }
+}
