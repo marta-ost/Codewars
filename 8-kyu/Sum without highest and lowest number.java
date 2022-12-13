f9a83d0351d3a9cd2b4575f0f3ba548973354746
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-13 13:50:39 +0100

    refactor Sum without highest and lowest number.java

--- 8-kyu/Sum without highest and lowest number.java
+++ 8-kyu/Sum without highest and lowest number.java
@@ -18,11 +18,11 @@ import java.util.Arrays;
 
 public class Kata {
   public static int sum(int[] numbers) {
-    if (numbers == null || numbers.length == 0) {
+    if (numbers == null || numbers.length == 0 || numbers.length == 1) {
       return 0;
     }
     Arrays.sort(numbers);
-    int sum = 0;
+    int sum= 0;
     for (int i = 1; i < numbers.length - 1; i++) {
       sum += numbers[i];
     }
