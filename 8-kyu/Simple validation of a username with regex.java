a4ab93d929fe6eb987b67a7f88eb04763ed8324a
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-15 16:02:14 +0100

    refactor Simple validation of a username with regex.java

--- 8-kyu/Simple validation of a username with regex.java
+++ 8-kyu/Simple validation of a username with regex.java
@@ -10,8 +10,6 @@ Length should be between 4 and 16 characters (both included).
 
 public class ZywOo {
   public static boolean validateUsr(String s) {
-    return s.matches("[a-z0-9_]+") 
-      && s.length() >= 4 
-      && s.length() <= 16;
+    return s.matches("[a-z0-9_]{4,16}");
   }
 }
