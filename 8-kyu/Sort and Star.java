1c4a8ce30ec514db6d66429dee640ac32b094b10
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-15 17:53:13 +0100

    refactor Sort and Star.java

--- 8-kyu/Sort and Star.java
+++ 8-kyu/Sort and Star.java
@@ -8,16 +8,10 @@ You should not remove or add elements from/to the array.
 */
 
 import java.util.Arrays;
-import java.util.stream.*;
 
 public class SortAndStar {
   public static String twoSort(String[] s) {
     Arrays.sort(s);
-    return s[0]
-      .chars()
-      .mapToObj(x -> String.valueOf((char) x))
-      .map(x -> x + "***")
-      .collect(Collectors.joining())
-      .substring(0, s[0].length() * 4 - 3);
+    return String.join("***", s[0].split(""));
   }
 }
