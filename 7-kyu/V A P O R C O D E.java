62d70dae834a201899f4d1647cfcc67d28ab9d02
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-18 17:08:27 +0100

    create V A P O R C O D E.java

---
+++ 7-kyu/V A P O R C O D E.java
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+ASC Week 1 Challenge 4 (Medium #1)
+
+Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+Note that spaces should be ignored in this case.
+
+Examples
+"Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+"Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
+*/
+
+import java.util.stream.*;
+
+public class Solution {
+  public static String vaporcode(String s) {
+    return s
+      .chars()
+      .mapToObj(ch -> (char) ch)
+      .filter(ch -> ch != ' ')
+      .map(String::valueOf)
+      .map(ch -> ch.toUpperCase())
+      .collect(Collectors.joining("  "));
+  }
+}
