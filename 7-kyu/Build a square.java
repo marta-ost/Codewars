f9ef4e1fbd7fb893806bd682d842735db40fa2d9
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-18 19:59:39 +0100

    create Build a square.java

---
+++ 7-kyu/Build a square.java
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+*/
+
+public class Kata {
+  public static final String generateShape(int n) {
+    return ("+".repeat(n) + "\n").repeat(n).substring(0, (n + 1) * n - 1);
+  }
+}
