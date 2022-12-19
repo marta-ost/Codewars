8f0f594480bb881c1061294808e62747b188bfda
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-19 15:05:08 +0100

    create Sort arrays - 1.java

---
+++ 7-kyu/Sort arrays - 1.java
@@ -0,0 +1,33 @@
+/*
+DESCRIPTION:
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+import java.util.Arrays;
+
+public class SortArray {
+  public static Integer[] sortArray(Integer[] array){
+    Arrays.sort(array);
+    return array;
+  }
+  public static String[] sortArray(String[] array){
+    Arrays.sort(array);
+    return array;
+  }
+  public static int[] sortArray(int[] array){
+    Arrays.sort(array);
+    return array;
+  }
+  public static long[] sortArray(long[] array){
+    Arrays.sort(array);
+    return array;
+  }
+  public static float[] sortArray(float[] array){
+    Arrays.sort(array);
+    return array;
+  }
+  public static double[] sortArray(double[] array){
+    Arrays.sort(array);
+    return array;
+  }
+}
