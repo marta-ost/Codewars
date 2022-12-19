11231340317c38034f3fc33d2765e368a3f30852
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-19 15:47:51 +0100

    refactor Shortest Word.java

--- 7-kyu/Shortest Word.java
+++ 7-kyu/Shortest Word.java
@@ -5,15 +5,13 @@ Simple, given a string of words, return the length of the shortest word(s).
 String will never be empty and you do not need to account for different data types.
 */
 
+import java.util.stream.*;
+
 public class Kata {
   public static int findShort(String s) {
-    String[] words = s.split(" ");
-    int minLength = Integer.MAX_VALUE;
-    for (String word : words) {
-      if (word.length() < minLength) {
-        minLength = word.length();
-      }
-    }
-    return minLength;
+    return Stream.of(s.split(" "))
+          .mapToInt(String::length)
+          .min()
+          .getAsInt();
   }
 }
