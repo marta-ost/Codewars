674a3c37afa210f8799e495e92c163c0354567e1
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-19 14:51:06 +0100

    create Is this a triangle.java

---
+++ 7-kyu/Is this a triangle.java
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+*/
+
+class TriangleTester{
+  public static boolean isTriangle(int a, int b, int c){
+    return a + b > c && b + c > a && c + a > b;
+  }
+}
