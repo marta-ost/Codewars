86b26eef59df1902de27e7a64daa87237a1b1189
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-20 16:33:11 +0100

    create this is a problem.java

---
+++ 8-kyu/this is a problem.java
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION:
+We want to create a constructor function 'NameMe', which takes first name and last name as parameters. The function combines the first and last names and saves the value in "name" property.
+
+We already implemented that function, but when we actually run the code, the "name" property is accessible, but the "firstName" and "lastName" is not accessible. All the properties should be accessible. Can you find what's wrong with it? A test fixture is also available
+
+public class NameMe {
+    private String firstName;
+    private String lastName;
+    private String fullName;
+
+    public NameMe(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+   }
+ }
+
+NameMe nameMe = new NameMe("John", "Doe");
+nameMe.getFirstName(); //Expected: John
+nameMe.getLastName(); //Expected: Doe
+nameMe.getFullName(); //Expected: John Doe
+*/
+
+public class NameMe {
+  public String firstName;
+  public String lastName;
+  public String fullName;
+
+  public NameMe(String first, String last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.fullName = first + " " + last;
+  }
+}
