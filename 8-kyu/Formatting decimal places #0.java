e5ac8e6ff28941ea69926c5f3daf703c093cb450
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-20 18:17:26 +0100

    create Formatting decimal places #0.java

---
+++ 8-kyu/Formatting decimal places #0.java
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Example:    
+5.5589 is rounded 5.56   
+3.3424 is rounded 3.34
+*/
+
+import java.text.DecimalFormat;
+
+public class Numbers {
+  public static double TwoDecimalPlaces(double number) {
+    DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+    return Double.parseDouble(decimalFormat.format(number));
+  }
+}
