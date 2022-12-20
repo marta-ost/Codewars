9a24d36be140c08c932b6b7e9728ce3ae2fb58e9
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-20 18:22:03 +0100

    create Push a hash an object into array.java

---
+++ 8-kyu/Push a hash an object into array.java
@@ -0,0 +1,15 @@
+/*
+DESCRIPTION:
+You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+*/
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PushAnObjectIntoArray {  
+  public static List<String> push(){
+    List<String> items = new ArrayList<>();
+    items.add("an object");
+    return items;
+  }
+}
