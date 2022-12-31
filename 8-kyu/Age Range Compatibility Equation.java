6be081a3cfebe7ec63867363fa87c628a40d21e0
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2022-12-31 15:34:32 +0100

    refactor Age Range Compatibility Equation.java

--- 8-kyu/Age Range Compatibility Equation.java
+++ 8-kyu/Age Range Compatibility Equation.java
@@ -23,7 +23,7 @@ age = 17   =>   15-20
 public class Kata {
   public static String datingRange(int age) {
     return age <= 14 
-      ? (int) Math.floor(age - 0.10 * age) + "-" + (int) Math.floor(age + 0.10 * age)
-      : (int) Math.floor(age / 2 + 7) + "-" + (int) Math.floor((age - 7) * 2);
+      ? (int) (age - 0.10 * age) + "-" + (int) (age + 0.10 * age)
+      : (int) (age / 2 + 7) + "-" + (int) ((age - 7) * 2);
   }
 }
