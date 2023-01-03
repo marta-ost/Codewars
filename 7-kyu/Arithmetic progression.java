b4ca0f5b27eafc83ef473d86971d3d24cb3eeb26
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-01-03 17:16:20 +0100

    create Arithmetic progression.java

---
+++ 7-kyu/Arithmetic progression.java
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+In your class, you have started lessons about arithmetic progression. Since you are also a programmer, you have decided to write a function that will return the first n elements of the sequence with the given common difference d and first element a. Note that the difference may be zero!
+
+The result should be a string of numbers, separated by comma and space.
+
+Example
+# first element: 1, difference: 2, how many: 5
+arithmetic_sequence_elements(1, 2, 5) == "1, 3, 5, 7, 9"
+*/
+
+class Progression {
+  public static String arithmeticSequenceElements(int a, int d, long n) {
+    StringBuilder sequence = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      sequence.append(a).append(", ");
+      a += d;
+    }
+    return sequence.substring(0, sequence.length() - 2);
+  }
+}
