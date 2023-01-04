1ba263290380f7819f8fe602213805cb0fb4dc3b
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-01-04 18:15:15 +0100

    create Suzuki needs help lining up his students.java

---
+++ 7-kyu/Suzuki needs help lining up his students.java
@@ -0,0 +1,50 @@
+/*
+DESCRIPTION:
+Suzuki needs help lining up his students!
+
+Today Suzuki will be interviewing his students to ensure they are progressing in their training. He decided to schedule the interviews based on the length of the students name in descending order. The students will line up and wait for their turn.
+
+You will be given a string of student names. Sort them and return a list of names in descending order.
+
+Here is an example input:
+
+string = 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi'
+Here is an example return from your function:
+
+ lst = ['Takehiko',
+        'Takayuki',
+        'Takahiro',
+        'Takeshi',
+        'Takeshi',
+        'Takashi',
+        'Tadashi',
+        'Takeo',
+        'Takao']
+Names of equal length will be returned in reverse alphabetical order (Z->A) such that:
+
+string = "xxa xxb xxc xxd xa xb xc xd"
+Returns
+
+['xxd', 'xxc', 'xxb', 'xxa', 'xd', 'xc', 'xb', 'xa']
+Please also try the other Kata in this series..
+
+Help Suzuki count his vegetables...
+Help Suzuki purchase his Tofu!
+Help Suzuki pack his coal basket!
+Help Suzuki rake his garden!
+How many stairs will Suzuki climb in 20 years?
+*/
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Kata {
+  public static String[] lineupStudents(String students) {     
+    return Arrays
+      .stream(students.split(" "))
+      .sorted(Comparator
+              .comparingInt(String::length).reversed()
+              .thenComparing(Comparator.reverseOrder()))
+      .toArray(String[]::new);
+  }
+}
