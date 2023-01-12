7cf3298c72c13aad598c89ca86e3b29428f0909a
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-01-12 10:48:01 +0100

    refactor Break camelCase.java

--- 6-kyu/Break camelCase.java
+++ 6-kyu/Break camelCase.java
@@ -10,17 +10,6 @@ Example
 
 class Solution {
   public static String camelCase(String input) {
-    StringBuilder broke = new StringBuilder();
-    int previous = 0;
-    
-    for (int i = 0; i < input.length(); i++) {
-      if (i - 1 >= 0 && String.valueOf(input.charAt(i)).matches("[A-Z]")) {
-        broke.append(input.substring(previous, i)).append(" ");
-        previous = i;
-      }
-    }
-    broke.append(input.substring(previous));
-    
-    return broke.toString();
+    return input.replaceAll("([A-Z])", " $1");
   }
 }
