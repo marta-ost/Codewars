1f213b07f6066a6feb52adde4cfe68b9ba098411
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-02-02 18:58:10 +0100

    refactor Simple string characters.java

--- 7-kyu/Simple string characters.java
+++ 7-kyu/Simple string characters.java
@@ -9,23 +9,13 @@ More examples in the test cases.
 Good luck!
 */
 
-import java.util.Arrays;
-
 public class Kata {
   public static int[] Solve(String word) {
-    int[] result = new int[4];
-    result[0] = (int) Arrays.stream(word.split(""))
-      .filter(letter -> letter.matches("[A-Z]"))
-      .count();
-    result[1] = (int) Arrays.stream(word.split(""))
-      .filter(letter -> letter.matches("[a-z]"))
-      .count();
-    result[2] = (int) Arrays.stream(word.split(""))
-      .filter(letter -> letter.matches("[0-9]"))
-      .count();
-    result[3] = (int) Arrays.stream(word.split(""))
-      .filter(letter -> letter.matches("[^a-zA-Z0-9_]"))
-      .count();
-    return result;  
+    return new int[] {
+        word.replaceAll("[^A-Z]", "").length(),
+        word.replaceAll("[^a-z]", "").length(),
+        word.replaceAll("[^0-9]", "").length(),
+        word.replaceAll("[A-Za-z0-9]", "").length()
+    };
   }
 }
