e2511703e82dd66a185dddfd921db37b62833bea
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-02-07 22:13:53 +0100

    create Small enough - Beginner.java

---
+++ 7-kyu/Small enough - Beginner.java
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+*/
+
+import java.util.Arrays;
+
+public class Kata {
+  public static boolean smallEnough(int[] a, int limit) {
+    return Arrays.stream(a)
+      .filter(number -> number <= limit)
+      .count() == a.length;
+  }
+}
