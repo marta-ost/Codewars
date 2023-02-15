2c0b77c075aff69d89bc78cac0c66c05440f64ef
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-02-15 18:27:06 +0100

    create Sort the odd.java

---
+++ 6-kyu/Sort the odd.java
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION:
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+*/
+
+import java.util.Arrays;
+
+public class Kata {
+  public static int[] sortArray(int[] array) {
+    int[] odd = Arrays.stream(array)
+      .filter(num -> num % 2 == 1)
+      .sorted()
+      .toArray();
+    
+    int oddIndex = 0;
+    for (int index = 0; index < array.length; index++) {
+      if (array[index] % 2 == 1) {
+        array[index] = odd[oddIndex];
+        oddIndex++;
+      }
+    }
+    return array;
+  }
+}
