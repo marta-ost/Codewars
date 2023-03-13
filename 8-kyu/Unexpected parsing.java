87de31ecdb0fbf876e1f544267bb56634981288a
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-03-13 20:14:23 +0100

    create Unexpected parsing.java

---
+++ 8-kyu/Unexpected parsing.java
@@ -0,0 +1,37 @@
+/*
+DESCRIPTION:
+Here is a piece of code:
+
+public class Kata {
+
+    public static HashMap <String, String> getStatus(boolean isBusy) {
+        HashMap<String, String> status;
+      
+        if (isBusy) {
+          status = "busy";
+        } else {
+          status = "available";
+        }
+
+        return status;
+    }
+}
+Expected Behaviour
+Function should return a dictionary/Object/Hash with "status" as a key, whose value can : "busy" or "available" depending on the truth value of the argument is_busy.
+
+But as you will see after clicking RUN or ATTEMPT this code has several bugs, please fix them.
+*/
+
+import java.util.HashMap;
+
+public class Kata {
+  public static HashMap <String, String> getStatus(boolean isBusy) {    
+    return isBusy ? 
+      new HashMap<>(){{
+        put("status", "busy");
+      }} : 
+      new HashMap<>(){{ 
+        put("status","available"); 
+      }};
+  }
+}
