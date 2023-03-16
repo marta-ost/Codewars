43154e619e59c9a8c9ef94758cf1462bb65b528b
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-03-16 20:48:41 +0100

    create Return the first M multiples of N.java

---
+++ 7-kyu/Return the first M multiples of N.java
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+Ex.
+
+multiples(3, 5.0)
+should return
+
+[5.0, 10.0, 15.0]
+*/
+
+import java.util.stream.*;
+
+public class Kata {
+  public static int[] multiples(int multiples, int number) {
+    return IntStream.range(1, multiples + 1)
+      .map(i -> i * number)
+      .toArray();
+  }
+}
