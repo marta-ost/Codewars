80b28febc0031dda8a54608cde68d32d58361465
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-03-18 15:52:14 +0100

    create max diff - easy.java

---
+++ 7-kyu/max diff - easy.java
@@ -0,0 +1,22 @@
+/*
+DESCRIPTION:
+You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+lst contains integers, that means it may contain some negative numbers
+if lst is empty or contains a single element, return 0
+lst is not sorted
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+Have fun!
+*/
+
+import java.util.Arrays;
+
+public class Kata {
+  public static int maxDiff(int[] lst) {
+    if (lst.length == 0)
+      return 0;
+    Arrays.sort(lst);
+    return lst[lst.length - 1] - lst[0];
+  }
+}
