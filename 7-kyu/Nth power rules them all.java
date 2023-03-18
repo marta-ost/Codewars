0cd57100bb296d0976c3f59a7053709a7f001b33
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-03-18 17:48:47 +0100

    refactor Nth power rules them all.java

--- 7-kyu/Nth power rules them all.java
+++ 7-kyu/Nth power rules them all.java
@@ -14,9 +14,7 @@ import java.util.Arrays;
 public class Kata {
   public static int modifiedSum(int[] array, int power) {
     return Arrays.stream(array)
-      .map(i -> (int) Math.pow(i, power))
-      .sum() -
-      Arrays.stream(array)
+      .map(i -> (int) Math.pow(i, power) - i)
       .sum();
   }
 }
