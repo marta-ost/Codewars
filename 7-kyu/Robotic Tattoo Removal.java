f85e4c30402c8f1f4f633c35ce343aa5d535d83b
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-03-25 19:16:02 +0100

    create Robotic Tattoo Removal.java

---
+++ 7-kyu/Robotic Tattoo Removal.java
@@ -0,0 +1,59 @@
+/*
+DESCRIPTION:
+Sometimes people get tattoos, sometimes they wish they hadn't, sometimes the tattoos must go. Lets create a robot that can remove tattoos.
+
+Your robot function accepts one array argument called skinScan. I have supplied an example array below.
+
+[
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ','X','X',' ',' ',' ','X','X',' ',' '],
+[' ','X',' ',' ','X',' ','X',' ',' ','X',' '],
+[' ','X',' ',' ',' ','X',' ',' ',' ','X',' '],
+[' ','X',' ',' ',' ','X',' ',' ',' ','X',' '],
+[' ','X',' ',' ',' ',' ',' ',' ',' ','X',' '],
+[' ','X',' ',' ',' ',' ',' ',' ',' ','X',' '],
+[' ','X',' ',' ',' ',' ',' ',' ',' ','X',' '],
+[' ',' ','X',' ',' ',' ',' ',' ','X',' ',' '],
+[' ',' ',' ','X',' ',' ',' ','X',' ',' ',' '],
+[' ',' ',' ',' ','X',' ','X',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ','X',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ','X',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+]
+Your task is to create a function for the robot function that will zap away the Xs and replace them with *s. Any array values that are not Xs must be left alone. Below is what skinScan should look like after the tattoo has been removed.
+
+[
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ','*','*',' ',' ',' ','*','*',' ',' '],
+[' ','*',' ',' ','*',' ','*',' ',' ','*',' '],
+[' ','*',' ',' ',' ','*',' ',' ',' ','*',' '],
+[' ','*',' ',' ',' ','*',' ',' ',' ','*',' '],
+[' ','*',' ',' ',' ',' ',' ',' ',' ','*',' '],
+[' ','*',' ',' ',' ',' ',' ',' ',' ','*',' '],
+[' ','*',' ',' ',' ',' ',' ',' ',' ','*',' '],
+[' ',' ','*',' ',' ',' ',' ',' ','*',' ',' '],
+[' ',' ',' ','*',' ',' ',' ','*',' ',' ',' '],
+[' ',' ',' ',' ','*',' ','*',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ','*',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ','*',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+]
+*/
+
+public class RoboticTattooRemoval {
+  public static String[][] robot(String[][] skinScan) {
+    final String TATTOOED = "X";
+    final String ZAPPED = "*";
+    
+    for (int i = 0; i < skinScan.length; i++)
+      for (int j = 0; j < skinScan[i].length; j++)
+        if (skinScan[i][j].equals(TATTOOED))
+          skinScan[i][j] = ZAPPED;
+    
+    return skinScan;
+  }
+}
