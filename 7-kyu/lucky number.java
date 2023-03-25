fe4089113b1057cbcbbeae40fd3c566ad0aedb77
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-03-25 18:11:47 +0100

    create lucky number.java

---
+++ 7-kyu/lucky number.java
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+###Lucky number
+
+Write a function to find if a number is lucky or not. If the sum of all digits is 0 or multiple of 9 then the number is lucky.
+
+1892376 => 1+8+9+2+3+7+6 = 36. 36 is divisible by 9, hence number is lucky.
+
+Function will return true for lucky numbers and false for others.
+*/
+
+import java.util.Arrays;
+
+public class LuckyNumber {
+  public static boolean isLucky(long n) {
+    int sumOfDigits = Arrays.stream(String.valueOf(n).split(""))
+      .mapToInt(Integer::parseInt)
+      .sum();
+    return sumOfDigits == 0 || sumOfDigits % 9 == 0;
+  }
+}
