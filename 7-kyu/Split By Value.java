db4952b90830c25366334e41b01dbc3bdeab4152
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-03-27 12:28:22 +0200

    create Split By Value.java

---
+++ 7-kyu/Split By Value.java
@@ -0,0 +1,50 @@
+/*
+DESCRIPTION:
+For an integer k rearrange all the elements of the given array in such way, that:
+
+all elements that are less than k are placed before elements that are not less than k;
+all elements that are less than k remain in the same order with respect to each other;
+all elements that are not less than k remain in the same order with respect to each other.
+
+For k = 6 and elements = [6, 4, 10, 10, 6], the output should be splitByValue(k, elements) = [4, 6, 10, 10, 6].
+
+For k = 5 and elements = [1, 3, 5, 7, 6, 4, 2], the output should be splitByValue(k, elements) = [1, 3, 4, 2, 5, 7, 6].
+
+S: codefights.com
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+  int[] splitByValue(int k, int[] elements) {
+    int smallerCount = (int) Arrays.stream(elements)
+      .filter(elem -> elem < k)
+      .count();
+    int biggerCount = elements.length - smallerCount;
+    
+    int[] smaller = new int[smallerCount];
+    int[] bigger = new int[biggerCount];
+    
+    int smallerIndex = 0;
+    int biggerIndex = 0;
+    
+    for (int i = 0; i < elements.length; i++) {
+      if (elements[i] < k)
+        smaller[smallerIndex++] = elements[i];
+      else
+        bigger[biggerIndex++] = elements[i];
+    }
+    
+    int[] rearranged = new int[elements.length];
+    
+    for (int i = 0; i < smaller.length; i++)
+      rearranged[i] = smaller[i];
+    
+    biggerIndex = 0;
+    
+    for (int i = smaller.length; i < rearranged.length; i++)
+      rearranged[i] = bigger[biggerIndex++];
+    
+    return rearranged;
+  }
+}
