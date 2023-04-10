4060bdebd014b4dfbf3888f5bfe1e473bfdf0bee
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-04-10 20:54:55 +0200

    create Surface Area and Volume of a Box.java

---
+++ 8-kyu/Surface Area and Volume of a Box.java
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+*/
+
+public class Kata {
+  public static int[] getSize(int w, int h, int d) {
+    int surfaceArea = 2 * (w * h + h * d + w * d);
+    int volume = w * h * d;
+    
+    return new int[]{surfaceArea, volume};
+  }
+}
