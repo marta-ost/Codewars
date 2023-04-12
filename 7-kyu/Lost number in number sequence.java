1a04d5a35a7700a82723f44810f13443815e8fe7
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-04-12 22:16:11 +0200

    create Lost number in number sequence.java

---
+++ 7-kyu/Lost number in number sequence.java
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION:
+An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+
+If no number was deleted from the starting array, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []).
+*/
+
+import java.util.Arrays;
+
+public class Kata {
+  public static int findDeletedNumber(int[] arr, int[] mixedArr) {
+    if (mixedArr.length == arr.length)
+      return 0;
+    
+    if (arr.length <= 1)
+      return arr.length;
+    
+    Arrays.sort(mixedArr);
+
+    for (int i = 0; i < mixedArr.length; i++)
+      if (arr[i] != mixedArr[i])
+        return arr[i];
+    
+    return arr[arr.length - 1];
+  }
+}
