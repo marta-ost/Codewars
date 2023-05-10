33f03a4f105f0dbd187b12b1ce1bf19ec077db68
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-05-10 14:23:42 +0200

    refactor Where is THB.java

--- 7-kyu/Where is THB.java
+++ 7-kyu/Where is THB.java
@@ -5,15 +5,9 @@ Help! THB has gone missing, and we need you to find him! You will be given a str
 Remember, you should return an empty string if you are given an empty string or a null string. Good luck!
 */
 
-import java.util.Arrays;
-import java.util.stream.*;
-
 public class THB {
   public static String testing(String initial) {
-    return initial == null || initial.isEmpty() ? "" 
-      : Arrays
-        .stream(initial.split(""))
-        .filter(letter -> letter.matches("[thbTHB]"))
-        .collect(Collectors.joining(""));
+    return initial == null ? "" 
+      : initial.replaceAll("[^thbTHB]", "");
   }
 } 
