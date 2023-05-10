fc0e853f51db832a171ba4bb0e3219cdf5e088f1
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-05-10 14:49:27 +0200

    create Triangles from Dots.java

---
+++ 7-kyu/Triangles from Dots.java
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+In this simple kata you need to find how many dots are needed to construct nth triangle in the sequence of dot triangles. See next image to get the idea of the sequence:
+
+sequence
+
+You need to write a function that will return a number dots needed to construct nth triangle.
+*/
+
+package codewars.triangle;
+
+import java.math.BigInteger;
+
+public class TriangularNumbers {
+  public static BigInteger numberOfDots(BigInteger n) {
+    final BigInteger TWO = new BigInteger("2");
+    
+    return n.multiply(n.add(BigInteger.ONE)).divide(TWO);
+  }
+}
