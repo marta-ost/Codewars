cbf802dc4e4a72bdaef250d1f1ec352cd606ad11
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-05-11 15:50:17 +0200

    create Suitcase packing.java

---
+++ 7-kyu/Suitcase packing.java
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION:
+Mr. Square is going on a holiday. He wants to bring 2 of his favorite squares with him, so he put them in his rectangle suitcase.
+
+Write a function that, given the size of the squares and the suitcase, return whether the squares can fit inside the suitcase.
+
+fit_in(a,b,m,n)
+a,b are the sizes of the 2 squares
+m,n are the sizes of the suitcase
+
+Example
+fit_in(1,2,3,2) should return True
+fit_in(1,2,2,1) should return False
+fit_in(3,2,3,2) should return False
+fit_in(1,2,1,2) should return False
+*/
+
+public class MrSquare {
+  public static Boolean fit_in(int a, int b, int m, int n) {
+    return (a + b <= n && Math.max(a, b) <= m) ||
+           (a + b <= m && Math.max(a, b) <= n);
+  }
+}
