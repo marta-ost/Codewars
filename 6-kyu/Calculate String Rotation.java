5ae8cde6b9d0e6e2976d7f4beff9130307350ef5
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-05-17 00:46:14 +0200

    create Calculate String Rotation.java

---
+++ 6-kyu/Calculate String Rotation.java
@@ -0,0 +1,39 @@
+/*
+DESCRIPTION:
+Write a function that receives two strings and returns n, where n is equal to the number of characters we should shift the first string forward to match the second. The check should be case sensitive.
+
+For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+
+If the second string isn't a valid rotation of the first string, the method returns -1.
+
+Examples:
+"coffee", "eecoff" => 2
+"eecoff", "coffee" => 4
+"moose", "Moose" => -1
+"isn't", "'tisn" => 2
+"Esham", "Esham" => 0
+"dog", "god" => -1
+*/
+
+public class CalculateRotation {
+  static int shiftedDiff(String first, String second) {
+    if (first.equals(second))
+      return 0;
+    
+    String firstPart = "";
+    String secondPart = "";
+    int rotation = 0;
+    
+    for (int index = 0; index < first.length(); index++) {
+      secondPart = first.substring(index);
+      if (second.indexOf(secondPart) == -1)
+        continue;
+      
+      firstPart = first.substring(0, index);
+      rotation = first.length() - index;
+      break;
+    }
+    
+    return (secondPart + firstPart).equals(second) ? rotation : -1;
+  }
+}
