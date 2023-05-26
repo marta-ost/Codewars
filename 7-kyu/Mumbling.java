4e096146768a3f93505e04076d352c478fa4b114
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-05-26 16:12:09 +0200

    create Mumbling.java

---
+++ 7-kyu/Mumbling.java
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION:
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+import java.util.stream.*;
+
+public class Accumul {
+  public static String accum(String string) {
+    return IntStream
+      .range(0, string.length())
+      .mapToObj(i -> String.valueOf(string.charAt(i)).repeat(i + 1))
+      .map(s -> String.valueOf(s.charAt(0)).toUpperCase() + s.substring(1).toLowerCase())
+      .collect(Collectors.joining("-"));
+  }
+}
