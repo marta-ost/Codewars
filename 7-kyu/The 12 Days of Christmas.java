f88704037975f03e2b897544cc692ecd0664fa57
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-05-27 16:02:46 +0200

    refactor The 12 Days of Christmas.java

--- 7-kyu/The 12 Days of Christmas.java
+++ 7-kyu/The 12 Days of Christmas.java
@@ -33,20 +33,27 @@ import java.util.*;
 
 public class Dinglemouse {
 
-  public static class HelpSaveChristmas implements Comparator<String> {
-  
-    @Override
-    public int compare(final String line1, final String line2) {
-      String line1firstWord = line1.split(" ")[0];
-      String line2firstWord = line2.split(" ")[0];
-      
-      return 
-        ((line1firstWord.charAt(0) == 'O' && line2firstWord.charAt(0) == 'a') ||
-         (line1firstWord.charAt(0) == 'O' && line2firstWord.matches("[0-9]+")) ||
-         (line1firstWord.matches("[0-9]+") && line2firstWord.charAt(0) == 'a') ||
-         (line1firstWord.matches("[0-9]+") && line2firstWord.matches("[0-9]+") 
-             && Integer.parseInt(line1firstWord) > Integer.parseInt(line2firstWord)))
-        ? -1 : 1;
-    }
+  public static class HelpSaveChristmas implements Comparator<String> { 
+	String[] linesArray = new String[] {
+		        "On the 12th day of Christmas my true love gave to me",
+		        "12 drummers drumming,",
+		        "11 pipers piping,",
+		        "10 lords a leaping,",
+		        "9 ladies dancing,",
+		        "8 maids a milking,",
+		        "7 swans a swimming,", 
+		        "6 geese a laying,", 
+		        "5 golden rings,", 
+		        "4 calling birds,",
+		        "3 French hens,", 
+		        "2 turtle doves and", 
+		        "a partridge in a pear tree."
+	};
+	List<String> lines = Arrays.asList(linesArray);     
+		
+	@Override
+	public int compare(final String line1, final String line2) {
+	  return lines.indexOf(line1) - lines.indexOf(line2);
+	}
   }
 }
