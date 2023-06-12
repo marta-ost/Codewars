cfeebfd15eb00997fd156b867aaa6e7087bed27c
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-06-12 23:19:38 +0200

    create Pillars.java

---
+++ 8-kyu/Pillars.java
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+*/
+
+public class Kata { 
+  public static int pillars(int numPill, int dist, int width) {
+    return numPill == 1 ? 0 
+      : (numPill - 2) * width + (numPill - 1) * dist * 100;
+  }
+}
