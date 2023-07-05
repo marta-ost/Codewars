57f95c35ebdc5f20dddf591fbf842e3c84822bc2
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-07-05 20:28:07 +0200

    create Halving Sum.java

---
+++ 7-kyu/Halving Sum.java
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+Task
+Given a positive integer n, calculate the following sum:
+
+n + n/2 + n/4 + n/8 + ...
+
+All elements of the sum are the results of integer division.
+
+Example
+25  =>  25 + 12 + 6 + 3 + 1 = 47
+*/
+
+public class HalvingSum {
+  int halvingSum(int n) {
+    int divider = 1;
+    int sum = 0;
+    
+    while (n / divider >= 1) {
+      sum += n / divider;
+      divider *= 2;
+    }
+    
+    return sum;
+  }
+}
