586457cc2923e85a431f63040e534bf74ed89715
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-07-12 22:20:20 +0200

    create Lario and Muigi Pipe Problem.java

---
+++ 8-kyu/Lario and Muigi Pipe Problem.java
@@ -0,0 +1,36 @@
+/*
+DESCRIPTION:
+Issue
+Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+Task
+Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+Example
+Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+*/
+
+import java.util.Arrays;
+import java.util.stream.*;
+
+public class Kata {
+  public static int[] pipeFix(int[] numbers) {
+    int min = Arrays
+      .stream(numbers)
+      .min()
+      .getAsInt();
+    
+    int max = Arrays
+      .stream(numbers)
+      .max()
+      .getAsInt();
+    
+    return IntStream
+      .rangeClosed(min, max)
+      .toArray();
+  }
+}
