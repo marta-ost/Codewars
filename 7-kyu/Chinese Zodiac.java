e063153e0436545c0ec97e0e2e49aa11208f2080
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-08-12 11:38:09 +0200

    create Chinese Zodiac.java

---
+++ 7-kyu/Chinese Zodiac.java
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION:
+The Chinese zodiac is a repeating cycle of 12 years, with each year being represented by an animal and its reputed attributes. The lunar calendar is divided into cycles of 60 years each, and each year has a combination of an animal and an element. There are 12 animals and 5 elements; the animals change each year, and the elements change every 2 years. The current cycle was initiated in the year of 1984 which was the year of the Wood Rat.
+
+Since the current calendar is Gregorian, I will only be using years from the epoch 1924. For simplicity I am counting the year as a whole year and not from January/February to the end of the year.
+
+##Task
+
+Given a year, return the element and animal that year represents ("Element Animal"). For example I'm born in 1998 so I'm an "Earth Tiger".
+
+animals (or $animals in Ruby) is a preloaded array containing the animals in order:
+
+['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat', 'Monkey', 'Rooster', 'Dog', 'Pig']
+
+elements (or $elements in Ruby) is a preloaded array containing the elements in order:
+
+['Wood', 'Fire', 'Earth', 'Metal', 'Water']
+
+Tell me your zodiac sign and element in the comments. Happy coding :)
+*/
+
+public class Kata extends Arrays {
+  public static String chineseZodiac(int year) {
+    final int START_YEAR = 1924;
+    final int ELEMENTS_CHANGE = 2;
+    
+    int yearFromStart = year - START_YEAR;
+    
+    int elemIndex = (yearFromStart / ELEMENTS_CHANGE) % elements.length;
+    int animalIndex = yearFromStart % animals.length;
+    
+    return elements[elemIndex] + " " + animals[animalIndex];
+  }
+}
