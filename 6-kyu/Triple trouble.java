efe1bf5456d02b16f69d8bc1aab336046b71cdc9
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-08-24 13:23:55 +0200

    refactor Triple trouble.java

--- 6-kyu/Triple trouble.java
+++ 6-kyu/Triple trouble.java
@@ -20,47 +20,14 @@ TripleDouble(12345, 12345) == 0
 TripleDouble(666789, 12345667) == 1
 */
 
-import java.util.*;
 import java.util.stream.*;
-import java.util.function.*;
 
 public class Kata {
   public static int TripleDouble(long num1, long num2) {
-    Map<Long, Long> digitsWithAmountsNum1 = getDigitsWithAmountsMap(num1);
-    List<Long> triplets = getRepeatingDigits(digitsWithAmountsNum1, 3, num1);
-    
-    Map<Long, Long> digitsWithAmountsNum2 = getDigitsWithAmountsMap(num2);
-    List<Long> doublets = getRepeatingDigits(digitsWithAmountsNum2, 2, num2);
-      
-    return triplets == null || doublets == null ? 0 
-      : LongStream
-          .rangeClosed(0, 9)
-          .filter(digit -> triplets.contains(digit))
-          .filter(digit -> doublets.contains(digit))
-          .count() > 0 ? 1 
-      : 0;
-  }
-
-  private static List<Long> getRepeatingDigits(Map<Long, Long> digitsWithAmounts, int repetitions, long num) {
-    return digitsWithAmounts
-      .entrySet()
-      .stream()
-      .filter(entry -> entry.getValue() >= repetitions)
-      .map(entry -> entry.getKey())
-      .filter(digit -> checkIfRepeatingDigitsAreStraight(num, digit, repetitions))
-      .collect(Collectors.toList());
-  }
-  
-  private static boolean checkIfRepeatingDigitsAreStraight(long num, long digit, int repetitions) {
-    return String.valueOf(num)
-                 .indexOf(String.valueOf(digit).repeat(repetitions)) >= 0;
-  }
-  
-  private static Map<Long, Long> getDigitsWithAmountsMap(long num) {
-    return Arrays
-      .stream(String.valueOf(num).split(""))
-      .map(s -> Long.parseLong(s))
-      .collect(Collectors.groupingBy(Function.identity(),
-                                     Collectors.counting()));
+    return IntStream
+      .rangeClosed(0, 9)
+      .filter(digit -> String.valueOf(num1).contains(String.valueOf(digit).repeat(3)) &&
+                       String.valueOf(num2).contains(String.valueOf(digit).repeat(2)))
+      .count() > 0 ? 1 : 0;
   }
 }
