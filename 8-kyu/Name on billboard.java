9ffc336b4395009facb6f30c0ba25c83c3b6c710
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-08-26 19:34:07 +0200

    create Name on billboard.java

---
+++ 8-kyu/Name on billboard.java
@@ -0,0 +1,21 @@
+/*
+DESCRIPTION:
+You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for Java).
+
+You can not use multiplier "*" operator.
+
+If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).
+*/
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Billboard {
+	public static int billboard(String name, int price) {
+    return name.isEmpty() ? 0 
+      : Arrays
+        .stream(name.split(""))
+        .mapToInt(letter -> price)
+        .sum();
+  }
+}
