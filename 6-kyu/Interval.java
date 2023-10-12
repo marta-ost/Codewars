52a002f85304375cac8929fa8941f0639176e272
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-10-12 14:07:55 +0200

    refactor Interval.java

--- 6-kyu/Interval.java
+++ 6-kyu/Interval.java
@@ -24,30 +24,16 @@ public class Math {
   public static int[] Interval(int[] arr, String str) {
     if (str.isEmpty() || arr.length == 0) return new int[]{};
     
-    int firstNumber = findNumber(str.substring(1, str.indexOf(",")));
-    int lastNumber = findNumber(str.substring(str.indexOf(",") + 1));
+    int firstNumber = Integer.parseInt(str.substring(1, str.indexOf(",")));
+    int lastNumber = Integer.parseInt(str.substring(str.indexOf(",") + 1, str.length() - 1));
     
-    boolean startClosed = str.charAt(0) == '[';
-    boolean endClosed = str.charAt(str.length() - 1) == ']';  
-    
-    if (!startClosed) firstNumber++;
-    if (!endClosed) lastNumber--;
+    final int minNumber = str.startsWith("[") ? firstNumber : firstNumber + 1;
+    final int maxNumber = str.endsWith("]") ? lastNumber : lastNumber - 1;
   
-    return IntStream
-      .rangeClosed(firstNumber, lastNumber)
-      .boxed()
-      .filter(number -> Arrays.stream(arr).anyMatch(number::equals))
-      .mapToInt(o -> (int) o)
+    return Arrays
+      .stream(arr)
+      .filter(i -> i >= minNumber && i <= maxNumber)
+      .sorted()
       .toArray();
-  }
-  
-  private static int findNumber(String str) {
-    boolean positive = true;
-    if (str.charAt(0) == '-') positive = false;
-    
-    String numberAbsStr = str.replace("[", "").replace("]", "").replace("(", "").replace(")", "").replace("-", "");
-    int numberAbs = Integer.parseInt(numberAbsStr); 
-    
-    return positive ? numberAbs : -1 * numberAbs; 
   }
 }
