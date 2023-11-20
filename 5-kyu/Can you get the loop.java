cd6d569abc42664aecf6dbd17e4a699d914f6276
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-11-20 05:13:03 +0100

    create Can you get the loop.java

---
+++ 5-kyu/Can you get the loop.java
@@ -0,0 +1,44 @@
+/*
+DESCRIPTION:
+You are given a node that is the beginning of a linked list. This list contains a dangling piece and a loop. Your objective is to determine the length of the loop.
+
+For example in the following picture the size of the dangling piece is 3 and the loop size is 12:
+
+image
+
+// Use the `getNext()` method to get the following node.
+node.getNext()
+
+Notes:
+
+do NOT mutate the nodes!
+in some cases there may be only a loop, with no dangling piece
+
+Thanks to shadchnev, I broke all of the methods from the Hash class.
+
+Don't miss dmitry's article in the discussion after you pass the Kata !! 
+*/
+
+import java.util.*;
+
+public class LoopInspector {
+  public int loopSize(Node node) {
+    List<Node> nodes = new ArrayList<>();
+    Node currentNode = node;
+    Node lastNode = null;
+    
+    while (lastNode == null) {
+      if (!nodes.contains(currentNode)) 
+        nodes.add(currentNode);
+      else
+        lastNode = currentNode;
+      
+      currentNode = currentNode.getNext();
+    }
+
+    Node firstNode = lastNode.getNext();
+    
+    return firstNode == lastNode ? 1 
+       : nodes.size() - (nodes.indexOf(firstNode) - 1);
+  }
+}
