6ddac845b33da7fac07b2625c15f54f9332a2515
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2023-12-04 17:51:27 +0100

    create Get Planet Name By ID.java

---
+++ 8-kyu/Get Planet Name By ID.java
@@ -0,0 +1,43 @@
+/*
+DESCRIPTION:
+The function is not returning the correct values. Can you figure out why?
+
+Example (Input --> Output ):
+
+3 --> "Earth"
+*/
+
+class Planet {
+  static String getPlanetName(int id) {
+    String name = "";
+    
+    switch (id) {
+      case 1:
+        name = "Mercury";
+        break;
+      case 2:
+        name = "Venus";
+        break;
+      case 3:
+        name = "Earth";
+        break;
+      case 4:
+        name = "Mars";
+        break;
+      case 5:
+        name = "Jupiter";
+        break;
+      case 6:
+        name = "Saturn";
+        break;
+      case 7:
+        name = "Uranus";
+        break;
+      case 8:
+        name = "Neptune";
+        break;
+    }
+    
+    return name;
+  }
+}
