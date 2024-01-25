e0034cc4139b5abf0206024455ea54cc2248d7a5
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-01-25 20:16:16 +0100

    create Multiple implications.java

---
+++ 7-kyu/Multiple implications.java
@@ -0,0 +1,27 @@
+/*
+DESCRIPTION:
+In logic, an implication (or material conditional) states that If p is true, q should be true too.
+
+We can express the result of any implication of two statements as a logical table:
+
+p\q T F      
+T   T F
+F   T T
+
+In this kata, we will take that further. Given an array (return false if the array is empty), assume that from first to last item in the array, each implies the next (for example, in an array of three items, p, q, and r: (p->q)->r. Return the boolean answer.
+
+Assume that there will be no more than 8 variables in the array, and the array will contain only boolean values.
+*/
+
+public class Logic {
+  public static Boolean multImplication(boolean[] lst) {
+    if (lst.length == 0) return false;
+    
+    boolean previousAnswer = lst[0];
+    
+    for (int i = 1; i < lst.length; i++) 
+      previousAnswer = !previousAnswer || (previousAnswer && lst[i]); 
+    
+    return previousAnswer;
+  }
+}
