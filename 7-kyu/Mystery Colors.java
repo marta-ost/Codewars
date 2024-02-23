37936dbf1264eaa919458ee5bcfeecb8685ecfb7
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-02-23 17:00:35 +0100

    refactor Mystery Colors.java

--- 7-kyu/Mystery Colors.java
+++ 7-kyu/Mystery Colors.java
@@ -39,22 +39,16 @@ public enum Color {
 }
 */
 
-import java.util.List;
+import java.util.*;
 
 public class MysteryColorAnalyzerImpl implements MysteryColorAnalyzer {
   @Override
   public int numberOfDistinctColors(List<Color> mysteryColors) {  
-    return (int) mysteryColors
-      .stream()
-      .distinct()
-      .count();
+    return new HashSet<Color>(mysteryColors).size();
   }
   
   @Override
   public int colorOccurrence(List<Color> mysteryColors, Color color) {
-    return (int) mysteryColors
-      .stream()
-      .filter(color::equals)
-      .count();
+    return Collections.frequency(mysteryColors, color);
   }
 }
