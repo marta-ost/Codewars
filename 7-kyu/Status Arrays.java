15e0f8076c70c92fe9c0b0fc9e2f445a41095fe9
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-03-02 03:23:56 +0100

    create Status Arrays.java

---
+++ 7-kyu/Status Arrays.java
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION:
+The status of each element of an array of integers can be determined by its position in the array and the value of the other elements in the array. The status of an element E in an array of size N is determined by adding the position P, 0 <= P < N, of the element in the array to the number of array elements in the array that are less than E.
+
+For example, consider the array containing the integers: 6 9 3 8 2 3 1. The status of the element 8 is 8 because its position is 3 and there are 5 elements in the array that are less than 8.
+
+You will return the elements of the original array from low to high status order. In the event there is a tie for status of two or more elements, you will output them in order of appearance in the array.
+
+EXAMPLE:
+
+status([6, 9, 3, 8, 2, 3, 1]) = [6, 3, 2, 1, 9, 3, 8]
+*/
+
+import java.util.stream.*;
+import java.util.*;
+
+public class Rays {
+  public static int[] status(int[] nums) {
+    return IntStream
+      .range(0, nums.length)
+      .boxed()
+      .sorted((i1, i2) -> Integer.compare(i1 + findCountOfElemsSmallerThanElemAtIndex(nums, i1),
+                                          i2 + findCountOfElemsSmallerThanElemAtIndex(nums, i2)))
+      .mapToInt(i -> nums[i])
+      .toArray();
+  }
+  
+  private static int findCountOfElemsSmallerThanElemAtIndex(int[] nums, int index) {
+    return (int) IntStream
+      .range(0, nums.length)
+      .filter(i -> nums[i] < nums[index])
+      .count();
+    }
+}
