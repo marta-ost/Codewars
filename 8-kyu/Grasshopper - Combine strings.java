1f9716f9f26cc2e54f90a170e4678bd5fe909278
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-03-06 21:01:10 +0100

    create Grasshopper - Combine strings.java

---
+++ 8-kyu/Grasshopper - Combine strings.java
@@ -0,0 +1,19 @@
+/*
+DESCRIPTION:
+
+Combine strings function
+
+Example:
+
+combineNames('James', 'Stevens')
+
+returns:
+
+'James Stevens'
+*/
+
+public class StringCombiner {
+  public static String combineNames(String first, String last) {
+    return first + " " + last;
+  }
+}
