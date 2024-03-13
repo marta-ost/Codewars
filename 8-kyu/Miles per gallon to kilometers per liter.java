e677a0b1e55c2576610dbb2b4e4ef33eb9e94e3e
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-03-13 20:06:43 +0100

    create Miles per gallon to kilometers per liter.java

---
+++ 8-kyu/Miles per gallon to kilometers per liter.java
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION:
+Sometimes, I want to quickly be able to convert miles per imperial gallon (mpg) into kilometers per liter (kpl).
+
+Create an application that will display the number of kilometers per liter (output) based on the number of miles per imperial gallon (input).
+
+Make sure to round off the result to two decimal points.
+
+Some useful associations relevant to this kata:
+
+1 Imperial Gallon = 4.54609188 litres
+1 Mile = 1.609344 kilometres
+*/
+
+public class Converter {
+  static final float IMPERIAL_GALLON = 4.54609188f;
+  static final float MILE = 1.609344f;
+  
+  public static float mpgToKPM(final float mpg) {
+    float kpl = mpg * MILE / IMPERIAL_GALLON;
+    return Math.round(kpl * 100.0f) / 100.0f;
+  }
+}
