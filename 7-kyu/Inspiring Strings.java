cdb087a339723edede94e0e1f83c3638491afd56
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-03-20 12:30:04 +0100

    refactor Inspiring Strings.java

--- 7-kyu/Inspiring Strings.java
+++ 7-kyu/Inspiring Strings.java
@@ -10,23 +10,12 @@ Example:
 */
 
 import java.util.*;
-import java.util.stream.*;
 
 public class Kata {
   public static String longestWord(String wordString) {
-    String[] words = wordString.split(" ");
-    
-    int maxLength = Arrays
-      .stream(words)
-      .mapToInt(word -> word.length())
-      .max()
-      .getAsInt();
-      
-    return IntStream
-      .range(0, words.length)
-      .mapToObj(i -> words[words.length - i - 1])
-      .filter(word -> word.length() == maxLength)
-      .findFirst()
+    return Arrays
+      .stream(wordString.split(" "))
+      .reduce((word1, word2) -> word1.length() > word2.length() ? word1 : word2)
       .get();
   }
 }
