e4e7b994d040e8dc948ff9ee80e0430f7955f1ad
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-04-21 03:44:00 +0200

    create Sequences and Series.java

---
+++ 6-kyu/Sequences and Series.java
@@ -0,0 +1,28 @@
+/*
+DESCRIPTION:
+Have a look at the following numbers.
+
+ n | score
+---+-------
+ 1 |  50
+ 2 |  150
+ 3 |  300
+ 4 |  500
+ 5 |  750
+
+Can you find a pattern in it? If so, then write a function getScore(n)/get_score(n)/GetScore(n) which returns the score for any positive number n.
+
+Note Real test cases consists of 100 random cases where 1 <= n <= 10000
+*/
+
+public class Sequence {
+  public static long getScore(long n) {
+    long index = 0;
+    long score = 0;
+    
+    while (index < n)
+      score = score + ++index * 50;
+    
+    return score;
+  }
+}
