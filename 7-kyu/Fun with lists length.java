4a533f1b77c6e86d7a6d20d09258c7687fb22af3
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-04-25 16:50:49 +0200

    create Fun with lists length.java

---
+++ 7-kyu/Fun with lists length.java
@@ -0,0 +1,54 @@
+/*
+DESCRIPTION:
+
+Implement the method length, which accepts a linked list (head), and returns the length of the list.
+
+For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+
+The linked list is defined as follows:
+
+class Node {
+  public Object data;
+  public Node next;
+
+  Node(T data, Node next) {
+    this.data = data;
+    this.next = next;
+  }
+  
+  Node(T data) {
+    this(data, null);
+  }
+}
+
+Note: the list may be null and can hold any type of value.
+
+Good luck!
+
+This kata is part of fun with lists series:
+
+Fun with lists: length
+Fun with lists: indexOf
+Fun with lists: lastIndexOf
+Fun with lists: countIf
+Fun with lists: anyMatch + allMatch
+Fun with lists: filter
+Fun with lists: map
+Fun with lists: reduce
+*/
+
+class Solution {
+  static int length(Node head) {
+    if (head == null)
+      return 0;
+    
+    int length = 1;
+  
+    while (head.next != null) {
+      head = head.next;
+      length++;
+    }
+    
+    return length;
+  }
+}
