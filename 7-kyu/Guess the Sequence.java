0cefa44c97918d10dbd8c144ba8537dac99c3bb7
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-05-06 19:12:24 +0200

    refactor Guess the Sequence.java

--- 7-kyu/Guess the Sequence.java
+++ 7-kyu/Guess the Sequence.java
@@ -9,19 +9,15 @@ result = [1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9]
 Good luck!
 */
 
-import java.util.*;
 import java.util.stream.*;
 
 class JomoPipi {
   static int[] sequence(int x) {
     return IntStream
-      .range(1, 10)
-      .mapToObj(i -> IntStream
-           .rangeClosed(i * 10 - 1, i * 10 + 9)
-           .map(j -> j == i * 10 - 1 ? i : j)
-           .toArray())
-      .flatMapToInt(Arrays::stream)
-      .filter(j -> j <= x)
+      .rangeClosed(1, x)
+      .mapToObj(String::valueOf)
+      .sorted()
+      .mapToInt(Integer::parseInt)
       .toArray();
   } 
 }
