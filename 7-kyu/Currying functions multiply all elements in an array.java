fcb3ce7fd0e3ed74d751a4ee4fb98e163d86d9df
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-05-07 19:47:51 +0200

    create Currying functions multiply all elements in an array.java

---
+++ 7-kyu/Currying functions multiply all elements in an array.java
@@ -0,0 +1,26 @@
+/*
+DESCRIPTION:
+To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+Example:
+
+multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+
+You must not mutate the original array.
+
+Here's a nice Youtube video about currying, which might help you if this is new to you.
+*/
+
+import java.util.function.*;
+import java.util.*;
+
+public class Kata {
+  public static IntFunction<int[]> multiplyAll(int[] array) {
+    return multiplier -> Arrays
+      .stream(array)
+      .map(i -> i * multiplier)
+      .toArray();
+  }
+}
