86031da02a820c1447f0efe810c1bd331815bd08
Marta Ostrycharz <marta.ostrycharz5@gmail.com>
2024-05-08 19:44:06 +0200

    create Power of two.java

---
+++ 7-kyu/Power of two.java
@@ -0,0 +1,29 @@
+/*
+DESCRIPTION:
+Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+
+a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+
+You may assume the input is always valid.
+
+Examples
+PowerOfTwo.isPowerOfTwo(1024) // -> true
+PowerOfTwo.isPowerOfTwo(4096) // -> true
+PowerOfTwo.isPowerOfTwo(333)  // -> false
+
+Beware of certain edge cases - for example, 1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.
+*/
+
+public class PowerOfTwo {
+  public static boolean isPowerOfTwo(long n) {
+    double number = n;
+    
+    while (number >= 1) {
+      if (number == 1)
+        return true;
+      number /= 2;
+    }
+    
+    return false;
+  }
+}
